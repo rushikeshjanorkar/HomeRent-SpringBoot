b051f8edc88b2039a38f1dd3b5b073bc2b8505ff
rushikeshjanorkar <rushikeshjanorkar867@gmail.com>
2024-11-16 11:02:31 +0530

    update new code

--- src/main/java/SpringBoot/HomeRentApplication/Security/SecurityConfig.java
+++ src/main/java/SpringBoot/HomeRentApplication/Security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -19,14 +21,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/api/auth/register", "/api/auth/login","/api/auth/admin/**","/api/auth/admin/login" ,"/api/monthly-bills/**").permitAll()
+        http
+                .csrf(csrf -> csrf.disable())  // Disabling CSRF if you’re using a stateless API
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/admin/**", "/api/monthly-bills/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(); // for basic authentication
+                .httpBasic(withDefaults());  // Applying default settings for basic auth
+
         return http.build();
     }
 }
